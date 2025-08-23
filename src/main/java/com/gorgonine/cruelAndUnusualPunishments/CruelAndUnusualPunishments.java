856fac650d969f02254423910fb9bfbc3cb8d0fb
Poundcake <74396509+Poundcaked@users.noreply.github.com>
2025-08-23 00:14:27 -0700

    Fix build

--- src/main/java/com/gorgonine/cruelAndUnusualPunishments/CruelAndUnusualPunishments.java
+++ src/main/java/com/gorgonine/cruelAndUnusualPunishments/CruelAndUnusualPunishments.java
@@ -3,26 +3,22 @@ package com.gorgonine.cruelAndUnusualPunishments;
 import com.gorgonine.cruelAndUnusualPunishments.commands.GuiCommand;
 import com.gorgonine.cruelAndUnusualPunishments.gui.GuiManager;
 import com.gorgonine.cruelAndUnusualPunishments.punishments.Punishments;
-import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public final class CruelAndUnusualPunishments extends JavaPlugin {
 
     public static final String PLUGIN_ID = "cruelandunusualpunishments";
-    public static final Logger LOGGER = LoggerFactory.getLogger(PLUGIN_ID);
 
     @Override
     public void onEnable() {
         getCommand("punish").setExecutor(new GuiCommand());
         Punishments.addPunishments();
         GuiManager.init();
-
+        getLogger().info("Cruel And Unusual Punishments Loaded!");
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        getLogger().info("Cruel And Unusual Punishments Shutdown!");
     }
 }
